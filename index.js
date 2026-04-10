78c8e35e0e1810ddbd8c6b33b1b2d944c80582e5
prateek mishraaa <pm921670@gmail.com>
2026-04-10 13:24:29 +0530

    fix cors error 2

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+
 app.use(cors());
 
 // Mount routes - ORDER MATTERS! Put specific routes before general ones
